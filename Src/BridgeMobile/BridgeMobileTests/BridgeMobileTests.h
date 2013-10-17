6963fcd45ee3a3b8167cdf209d5aae5869ee33fd
Juan David Hincapié-Ramos <jhincapie@gmail.com>
2013-10-17 10:31:14 -0500

    Starting iPhone application

---
+++ Src/BridgeMobile/BridgeMobileTests/BridgeMobileTests.h
@@ -0,0 +1,13 @@
+//
+//  BridgeMobileTests.h
+//  BridgeMobileTests
+//
+//  Created by Juan David Hincapie Ramos on 10/17/13.
+//  Copyright (c) 2013 Juan David Hincapié-Ramos. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BridgeMobileTests : SenTestCase
+
+@end
